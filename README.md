55874d66168de78cf089acdb259764376261ac84
Dean Jackson <dino@apple.com>
2020-01-23 11:57:12 +1100

    Add a --dumpall to get all the glyphs

--- README.md
+++ README.md
@@ -8,15 +8,25 @@ Only works on macOS.
 
 Usage:
 
-`bash
+```bash
 node emoji-to-png.js string
-`
+```
 
 This will iterate over every glyph in the string, producing a PNG
 file for each (named after the codepoints contained in the glyph).
+
+## I want all the emojis
+
+```bash
+node emoji-to-png.js --dumpall
+```
+
+This will output a PNG file for every codepoint in the font. Note that
+this won't include the glyphs that are formed by multiple codepoints.
 
 ## Note
 
 - file names might include the zero-width joiner codepoint, which is a bit silly
 - file names really should use the Unicode character names e.g. "PILE-OF-POO"
-
+- have `dumpall` include all available combinations
+- provide an output directory for the files
--- emoji-to-png.js
+++ emoji-to-png.js
@@ -9,12 +9,49 @@ const emojiFont = "/System/Library/Fonts/Apple Color Emoji.ttc";
 
 function usage() {
     console.log(`Usage: node ${process.argv[1]} string`);
-    console.log("Where string is a collection of emoji.")
-    console.log("A PNG file for each emoji will be saved, with the filename being");
-    console.log("the unicode code point. e.g. 💩 -> 1F4A9.png");
+    console.log("    Where string is a collection of emoji.")
+    console.log("    A PNG file for each emoji will be saved, with the filename being");
+    console.log("    the unicode code point. e.g. 💩 -> 1F4A9.png");
+    console.log(`Usage: node ${process.argv[1]} --dumpall`);
+    console.log("    Save a PNG file for every emoji codepoint.")
 }
 
-async function saveImagesForEmoji(inputString) {
+async function saveImageForGlyph(glyph) {
+    const img = glyph.getImageForSize(2000);
+    if (!img) {
+        console.log(`No glyph for codepoint ${glyph.codePoints}`);
+        return;
+    }
+    const imageFileName = glyph.codePoints.map(codePoint => codePoint.toString(16).toUpperCase()).join("-") + ".png";
+    // FIXME: Get the character name
+    // http://unicode.org/Public/UNIDATA/UnicodeData.txt
+    console.log(`output -> ${imageFileName}`);
+    await writeDataToFile(imageFileName, img.data);
+}
+
+async function saveImagesForString(inputString, font) {
+    const run = font.layout(inputString);
+    for (let glyph of run.glyphs) {
+        await saveImageForGlyph(glyph);
+    }
+}
+
+async function saveImagesForEverything(font) {
+    for (codePoint of font.characterSet) {
+        if (font.hasGlyphForCodePoint(codePoint)) {
+            await saveImageForGlyph(font.glyphForCodePoint(codePoint));
+        } else {
+            console.log("No codepoint for " + codePoint);
+        }
+    }
+}
+
+async function main() {
+    if (process.argv.length < 3) {
+        usage();
+        process.exit(1);
+    }
+
     let fontCollection;
     try {
         fontCollection = await openFont(emojiFont);
@@ -22,27 +59,15 @@ async function saveImagesForEmoji(inputString) {
         console.log(`Can't open ${emojiFont}. Are you sure you're on macOS?`);
         process.exit(1);
     }
+
     const font = fontCollection.fonts[0];
-    const run = font.layout(inputString);
-    for (let glyph of run.glyphs) {
-        const imageFileName = glyph.codePoints.map(codePoint => codePoint.toString(16).toUpperCase()).join("-") + ".png";
-        // FIXME: Get the character name
-        // http://unicode.org/Public/UNIDATA/UnicodeData.txt
-        console.log(`output -> ${imageFileName}`);
-        const img = glyph.getImageForSize(2000);
-        await writeDataToFile(imageFileName, img.data);
+
+    if (process.argv[2] == "--dumpall") {
+        saveImagesForEverything(font);
+    } else {
+        saveImagesForString(process.argv[2], font);
     }
 }
 
-if (process.argv.length < 3) {
-    usage();
-    process.exit(1);
-}
-
-try {
-    saveImagesForEmoji(process.argv[2]);
-} catch (e) {
-    console.log("OOPS!");
-    console.log(e);
-}
+main();
 
